6376a04be990ac7dc77ca1460e45304c229954ce
jennifer-pecky <ugwojennifer7@gmail.com>
2023-04-07 16:49:23 +0100

    working on header

---
+++ bayside/src/componets/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

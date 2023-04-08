c53b95b6f10b54e38a944ac41d04cb3391e1f5fd
jennifer-pecky <ugwojennifer7@gmail.com>
2023-04-08 16:16:57 +0100

    addded hero section

--- bayside/src/componets/Header/Header.jsx
+++ bayside/src/componets/Header/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
   return (
     <header>
       <div className="bg-[#ffffff]">
-        <div className="flex px-[4rem] p-[1.3rem] items-center justify-between">
-          <div className="text-[36px] text-[#0d5eba] leading-[43px]">
-            <h1 className="font-bold">BAYSIDE FINE JEWELRY</h1>
+        <div className="flex items-center justify-between">
+          <div className=" lg:px-[4rem] p-[1.3rem]  text-[36px] text-[#0d5eba] leading-[43px] ">
+            <h1 className="font-bold w-[100%]">BAYSIDE FINE JEWELRY</h1>
           </div>
           <nav className="">
-            <ul className="flex space-x-12 font-bold leading-[19px] items-center">
+            <ul className="flex space-x-12 font-bold leading-[19px] p-5 items-center hidden md:flex">
               <li>
                 <a href="">Home</a>
               </li>
--- bayside/src/componets/Hero/Hero.jsx
+++ bayside/src/componets/Hero/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import img from '../../assets/images/Image.png';
+import Shop from '../Shop/Shop';
 
 const Hero = () => {
   return (
-    <div className="md:w-[100%]">
-      <img src={img} alt="" className="w-[1444px] h-[794px]" />
+    <div>
+      <div className="max-w-[100%] md:w-[100%]">
+        <img src={img} alt="" />
+      </div>
+      <Shop />
     </div>
   );
 };

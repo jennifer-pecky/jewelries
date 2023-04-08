b4ffaed197edc811b3bdc088f59e837606381138
jennifer-pecky <ugwojennifer7@gmail.com>
2023-04-08 12:15:50 +0100

    workingon hero section

--- bayside/src/componets/Header/Header.jsx
+++ bayside/src/componets/Header/Header.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import { BsBell } from 'react-icons/bs';
+import Hero from '../Hero/Hero';
 
 const Header = () => {
   return (
     <header>
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="bg-[#ffffff]">
+        <div className="flex px-[4rem] p-[1.3rem] items-center justify-between">
+          <div className="text-[36px] text-[#0d5eba] leading-[43px]">
+            <h1 className="font-bold">BAYSIDE FINE JEWELRY</h1>
+          </div>
+          <nav className="">
+            <ul className="flex space-x-12 font-bold leading-[19px] items-center">
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Shop</a>
+              </li>
+              <li>
+                <a href="">Services</a>
+              </li>
+              <li>
+                <a href="">About</a>
+              </li>
+              <li>
+                <a href="">Contact</a>
+              </li>
+              <BsBell />
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </nav>
       </div>
+      <Hero />
     </header>
   );
 };
---
+++ bayside/src/componets/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../../assets/images/Image.png';
+
+const Hero = () => {
+  return (
+    <div className="md:w-[100%]">
+      <img src={img} alt="" className="w-[1444px] h-[794px]" />
+    </div>
+  );
+};
+
+export default Hero;

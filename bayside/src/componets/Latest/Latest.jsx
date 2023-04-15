e749de07fb4f6daeeaace86b9a7ac455bc690f68
jennifer-pecky <ugwojennifer7@gmail.com>
2023-04-15 18:01:24 +0100

    updated latest section

--- bayside/src/componets/Latest/Latest.jsx
+++ bayside/src/componets/Latest/Latest.jsx
@@ -6,12 +6,19 @@ import img3 from '../../assets/images/latest3.png';
 const Latest = () => {
   return (
     <div className="lg:px-20 mt-14">
-      <div className="text-[#002D69] text-[30px] leading-[43px]">
-        <h1 className="font-bold max-w-[100%]">OUR LATEST JEWELRY</h1> <br />
-        <div className="lg:grid grid-cols-3  ">
+      <div className=" space-y-12 mb-7">
+        <h1 className=" text-[#002D69] text-[30px] leading-[43px] font-bold max-w-[100%]">
+          OUR LATEST JEWELRY
+        </h1>{' '}
+        <div className="lg:grid grid-cols-3 gap-16 ">
           <img src={img} alt="" />
           <img src={img2} alt="" />
           <img src={img3} alt="" />
+        </div>
+        <div className="flex justify-center">
+          <button className="text-[#fff] bg-[#002D69] lg:p-3 lg:w-[16%] hover:bg-black font-semibold p-3 whitespace-nowrap ">
+            VIEW GALLERY
+          </button>
         </div>
       </div>
     </div>
